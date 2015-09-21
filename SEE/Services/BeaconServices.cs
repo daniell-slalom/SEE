659945f271e45ce617c8e8df0f7e142e202cc4d5
daniel <sihekuang@gmail.com>
2015-09-20 18:46:30 -0700

    Aded beacon service file

---
+++ SEE/Services/BeaconServices.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace SEE
+{
+	public class BeaconServices: Service
+	{
+		public BeaconServices ()
+		{
+		}
+	}
+}
+
