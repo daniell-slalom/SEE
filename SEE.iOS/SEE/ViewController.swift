d53c1d750df3e3436162530762b9a37efba7c087
Daniel Lee <daniell@slalom.com>
2015-10-26 00:02:44 -0700

    now allowing monitoring beacon

--- SEE.iOS/SEE/ViewController.swift
+++ SEE.iOS/SEE/ViewController.swift
@@ -5,19 +5,44 @@
 //  Created by Daniel Lee on 10/25/15.
 //  Copyright © 2015 Daniel Lee. All rights reserved.
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ESTBeaconManagerDelegate {
 
+    let beaconManager = ESTBeaconManager()
+    let beaconRegion = CLBeaconRegion(proximityUUID: NSUUID(UUIDString: "b9407f30-f5f8-466e-aff9-25556b57fe6d")!, identifier: "region")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // 3. Set the beacon manager's delegate
+        self.beaconManager.delegate = self
+        // 4. We need to request this authorization for every beacon manager
+        self.beaconManager.requestAlwaysAuthorization()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.beaconManager.startRangingBeaconsInRegion(self.beaconRegion)
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.beaconManager.stopRangingBeaconsInRegion(self.beaconRegion)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
+    
+    func beaconManager(manager: AnyObject, didStartMonitoringForRegion region: CLBeaconRegion) {
+    }
+    
+    func beaconManager(manager: AnyObject, didRangeBeacons beacons: [CLBeacon], inRegion region: CLBeaconRegion) {
+        NSLog("asdf")
+    }
+    
+    func beaconManager(manager: AnyObject, monitoringDidFailForRegion region: CLBeaconRegion?, withError error: NSError) {
 
-
+    }
 }
 
