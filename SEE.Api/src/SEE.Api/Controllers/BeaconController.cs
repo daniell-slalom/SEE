19c39e7c9061e297d71235c375d5d02ea57e25f8
Daniel Lee <daniell@slalom.com>
2015-10-27 23:04:34 -0700

    finished the end point

---
+++ SEE.Api/src/SEE.Api/Controllers/BeaconController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNet.Mvc;
+using SEE.Api.Managers;
+using SEE.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEE.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class BeaconController: Controller
+    {
+        [HttpGet]
+        public IEnumerable<UserLocation> Get()
+        {
+            var results = UserLocationsManager.Instance.GetRecords();
+            return results;
+        }
+
+        [HttpPost]
+        public void Post([FromBody]UserLocation userLocation)
+        {
+            UserLocationsManager.Instance.Log(userLocation);
+        }
+
+    }
+}
---
+++ SEE.Api/src/SEE.Api/Managers/UserLocationsManager.cs
@@ -0,0 +1,37 @@
+﻿using SEE.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEE.Api.Managers
+{
+    public class UserLocationsManager
+    {
+        public static UserLocationsManager Instance = new UserLocationsManager();
+
+        public Dictionary<string, UserLocation> UserLocationRecords = new Dictionary<string, UserLocation>();
+        private UserLocationsManager()
+        {
+
+        }
+
+        public void Log(UserLocation userLocation)
+        {
+            if (UserLocationRecords.ContainsKey(userLocation.username))
+            {
+                UserLocationRecords[userLocation.username] = userLocation;
+            }
+            else
+            {
+                UserLocationRecords.Add(userLocation.username, userLocation);
+            }
+        }
+
+        public IEnumerable<UserLocation> GetRecords()
+        {
+            var items = this.UserLocationRecords.Select(r => r.Value).ToList();
+            return items;
+        }
+    }
+}
