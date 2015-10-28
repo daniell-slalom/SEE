b1bc90e3a0ed3f263695d9f4a238d021545a3366
Daniel Lee <daniell@slalom.com>
2015-10-27 22:52:47 -0700

    Added userlocation file

---
+++ SEE.Api/src/SEE.Api/Models/UserLocation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEE.Api.Models
+{
+    public class UserLocation
+    {
+        public string username { get; set; }
+        public string location { get; set; }
+        public string timeStamp { get; set; }
+    }
+}
