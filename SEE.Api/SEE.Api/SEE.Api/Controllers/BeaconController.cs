d3943fcf3f654be51ba044a55c1d1c76102c05c2
Daniel Lee <daniell@slalom.com>
2015-10-28 09:36:08 -0700

    downgrade the api

---
+++ SEE.Api/SEE.Api/SEE.Api/Controllers/BeaconController.cs
@@ -0,0 +1,27 @@
+﻿using SEE.Api.Managers;
+using SEE.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SEE.Api.Controllers
+{
+    public class BeaconController : ApiController
+    {
+        // GET: api/Beacon
+        public IEnumerable<UserLocation> Get()
+        {
+            var items = UserLocationsManager.Instance.GetRecords();
+            return items;
+        }
+
+        // PUT: api/Beacon/5
+        public void Put([FromBody]UserLocation userLocation)
+        {
+            UserLocationsManager.Instance.Log(userLocation);
+        }
+    }
+}
---
+++ SEE.Api/SEE.Api/SEE.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SEE.Api.WebApiApplication" Language="C#" %>
